57e0f7f79101a7e8b17e2773be7c58e048ea1926
Samardeep Kajal <samk712003@gmail.com>
2023-03-19 19:05:38 +0530

    fixed path and added errorHandler

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
 }
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
