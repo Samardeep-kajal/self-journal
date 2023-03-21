d916672df5afaf77596ecac7dda4b2d2fe2ea84c
Samardeep Kajal <samk712003@gmail.com>
2023-03-21 14:19:16 +0530

    Debugging server.js using console logs

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,18 @@ const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+console.log("Connecting to database....");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
-
+console.log("Database connected successfully");
 connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
+console.log("User routes initialized");
 app.use("/api/journals", require("./routes/journalRoutes"));
+console.log("journal routes are initialized");
 
 //Serve frontend/client
 if (process.env.NODE_ENV === "production") {
@@ -27,6 +30,8 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(errorHandler);
+console.log("Error handler is initialized");
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
+console.log("Server is listening on port ", port);
